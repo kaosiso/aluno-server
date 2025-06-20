9eaa5d751442aa0d55f39f279b503599a165b0fa
kaosiso <kaosisochukwum@gmail.com>
2025-06-20 17:04:27 +0100

    Comment out auth routes to debug 404 error

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cookieParser = require("cookie-parser");
 const connectDB = require('./db/connectDB');
 const { connectCloudinary } = require('./configs/cloudinary');
 
-const authRoutes = require('./routes/auth.route');
-// const courseRoutes = require('./routes/course.route');
-// const instructorRoutes = require('./routes/instructor.route');
-// const userRoutes = require('./routes/user.route');
+// const authRoutes = require('./routes/auth.route');
+const courseRoutes = require('./routes/course.route');
+const instructorRoutes = require('./routes/instructor.route');
+const userRoutes = require('./routes/user.route');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 // API Routes
-app.use("/api/auth", authRoutes);
-// app.use("/api/course", courseRoutes);
-// app.use("/api/instructor", instructorRoutes);
-// app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+app.use("/api/course", courseRoutes);
+app.use("/api/instructor", instructorRoutes);
+app.use("/api/user", userRoutes);
 
 
 app.get("/", (req, res) => {
